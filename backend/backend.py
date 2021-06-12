165b5aabb2d91efac41741194c064abccdc14d39
pgosar <pgosarsteam@gmail.com>
2021-06-12 11:50:19 -0500

    getting list of competing brands

--- backend/backend.py
+++ backend/backend.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+foodType = [
+    {
+        "name": "Drinks",
+        "brands": ["orange juice", "coffee", "milk"]
+    },
+    {
+        "name": "Cheese",
+        "brands": ["American", "Cheddar", "mozarella"]   
+    },
+    {
+        "name": "fruit",
+        "brands": ["apple", "banana", "peach"]
+    },
+]
+companyStats = []
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome To Sustainabrands"}
+
+@app.get('/competitors/{name}')
+def getCompetitors(name):
+    for food in foodType:
+        if food['name'] == name:
+            return food['brands']
+    return {"error": "Brands for specified food does not exist"}
+
+# @app.get('/stats/{company}')  WIP
+# def getStats(name):
+#     for food in foodType:
+#         if food['name'] == name:
+#             return food
