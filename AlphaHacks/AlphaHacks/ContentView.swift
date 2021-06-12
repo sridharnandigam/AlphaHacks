91a43475472d1a4e95b682740fbca369dcb46e06
Arpan Dhatt <ary.dhatt@gmail.com>
2021-06-12 16:00:41 -0500

    loading screen shows on capture

--- AlphaHacks/AlphaHacks/ContentView.swift
+++ AlphaHacks/AlphaHacks/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     @State private var image = UIImage(systemName: "cake")
     @State private var didTapCapture = false
+    @State private var showSheet = false
     
     var body: some View {
         ZStack {
-            CustomCameraView(image: $image, didTapCapture: $didTapCapture).ignoresSafeArea(.all, edges: .top)
+            if !showSheet {
+                CustomCameraView(image: $image, didTapCapture: $didTapCapture).ignoresSafeArea(.all, edges: .top)
+            }
             VStack {
                 Text("Take a Picture of Your Receipt or Product").font(.bold(.largeTitle)()).multilineTextAlignment(.center).shadow(color: Color.green, radius: 10, x: 0.0, y: 0.0).foregroundColor(.white)
                 Spacer()
@@ -21,15 +24,11 @@ struct ContentView: View {
         }.onChange(of: didTapCapture, perform: { value in
             if didTapCapture == true {
                 print("Image captured!")
-                
+                showSheet = true
                 didTapCapture = false
             }
+        }).sheet(isPresented: $showSheet, content: {
+            LoadingPage()
         })
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
