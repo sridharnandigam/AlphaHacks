33e1934b64c1af74e645d44ed9984108bc724141
Arpan Dhatt <ary.dhatt@gmail.com>
2021-06-12 16:49:17 -0500

    building components of results page

---
+++ AlphaHacks/AlphaHacks/ArticleCard.swift
@@ -0,0 +1,28 @@
+//
+//  ArticleCard.swift
+//  AlphaHacks
+//
+//  Created by Arpan Dhatt on 6/12/21.
+//
+
+import SwiftUI
+
+struct ArticleCard: View {
+    var article: Article
+    
+    var body: some View {
+        HStack {
+            ImageView(withURL: article.image_link).cornerRadius(10.0)
+            VStack(alignment: .leading) {
+                Text(article.article_title).font(.title2)
+                Text(article.description).font(.caption)
+            }
+        }.frame(height: 100)
+    }
+}
+
+struct ArticleCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleCard(article: Article(article_title: "Article Title 1", description: "Article Description 1 Article Description 1 Article Description 1 Article Description 1 Article Description 1 ", link: "https://google.com", image_link: "https://lp-cms-production.imgix.net/2021-01/Nasa%201.jpeg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850"))
+    }
+}
