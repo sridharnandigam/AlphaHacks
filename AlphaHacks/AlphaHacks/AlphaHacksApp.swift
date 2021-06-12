7748f7f48af2bc3b609b082db4d0315f5b315008
Arpan Dhatt <ary.dhatt@gmail.com>
2021-06-11 22:32:54 -0500

    Adding iOS app starter

---
+++ AlphaHacks/AlphaHacks/AlphaHacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphaHacksApp.swift
+//  AlphaHacks
+//
+//  Created by Arpan Dhatt on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AlphaHacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
